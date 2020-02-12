cddca158667b14a7728335fb5cb418208b0130cb
alaiyeshi025 <alaiyeshi025@foxmail.com>
2020-02-12 17:09:02 +0800

    Capture function modified

---
+++ autogit.bat
@@ -0,0 +1,9 @@
+@echo off
+@title automatically running git commands
+
+C:
+cd c:/Johnny/PersonalFiles/GitBlog
+
+git add .
+git commit -m "Daily commit "%date:~0,4%.%date:~5,2%.%date:~8,2%" "%time:~0,2%:%time:~3,2%:%time:~6,2%
+git push origin master
