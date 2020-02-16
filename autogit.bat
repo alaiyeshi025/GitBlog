edca4d67bbf8921748d84af25a1b086c19d7ff9f
Johnny Zhang <alaiyeshi025@foxmail.com>
2020-02-17 06:23:03 +0800

    Auto commit 2020-02-17	06:23:03

--- autogit.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@title automatically running git commands
-
-C:
-cd c:/Johnny/PersonalFiles/GitBlog
-
-git add .
-git commit -m "Auto commit %date:~0,4%.%date:~5,2%.%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
-git push origin master
