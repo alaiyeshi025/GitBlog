665e5cdc323d45430304ff6b1042864a5be037cd
alaiyeshi025 <alaiyeshi025@foxmail.com>
2020-02-17 08:46:59 +0800

    Auto commit 2020.02.17  8:46:58

--- autogit.bat
+++ autogit.bat
@@ -4,6 +4,7 @@
 C:
 cd c:/Johnny/PersonalFiles/GitBlog
 
+git pull origin master
 git add .
 git commit -m "Auto commit %date:~0,4%.%date:~5,2%.%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git push origin master
