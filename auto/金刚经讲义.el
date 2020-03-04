ea2b1809ddaf09061194e1c7d538b6b08d452ab4
Johnny Zhang <alaiyeshi025@foxmail.com>
2020-03-04 20:23:39 +0800

    Auto commit 2020-03-04 20:23:39

---
+++ auto/金刚经讲义.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "金刚经讲义"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "xeCJK")
+   (LaTeX-add-labels
+    "sec:org2c5a7e9"
+    "sec:org169889a"
+    "sec:orgbcc13ac"
+    "sec:orgff0515d"
+    "sec:org9760c8d"
+    "sec:org7725812"
+    "sec:org5c2afbe"
+    "sec:orgd5d1c5e"))
+ :latex)
+
